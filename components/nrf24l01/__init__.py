c57f5174689848655bcacdd67c57dd6b86dc685f
RomanM <roman.musienko@boosta.co>
2025-02-23 23:42:41 +0200

    chore: test2

--- components/nrf24l01/__init__.py
+++ components/nrf24l01/__init__.py
@@ -2,6 +2,7 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome import pins
 from esphome.const import CONF_ID
+from esphome.components import spi
 
 DEPENDENCIES = ['spi']
 AUTO_LOAD = ['spi']
@@ -14,6 +15,7 @@ CONF_GATEWAY_ADDRESS = "gateway_address"
 CONF_HUBS = "hubs"
 CONF_ADDRESS = "address"
 CONF_PIPE = "pipe"
+CONF_CHECK_INTERVAL = 'check_interval'
 
 # Створюємо namespace для компоненту
 nrf24_ns = cg.esphome_ns.namespace('nrf24l01')
@@ -37,13 +39,15 @@ CONFIG_SCHEMA = cv.Schema({
     cv.Required(CONF_CE_PIN): pins.gpio_output_pin_schema,
     cv.Required(CONF_CSN_PIN): pins.gpio_output_pin_schema,
     cv.Required(CONF_MODE): cv.enum(MODE_OPTIONS, upper=False),
+    cv.Optional(CONF_CHECK_INTERVAL, default='10s'): cv.positive_time_period_seconds,
     cv.Optional(CONF_HUBS): cv.ensure_list(HUB_SCHEMA),
     cv.Optional(CONF_GATEWAY_ADDRESS): cv.string_strict
-}).extend(cv.COMPONENT_SCHEMA)
+}).extend(cv.COMPONENT_SCHEMA).extend(spi.spi_device_schema())
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
+    await spi.register_spi_device(var, config)
     
     # Налаштування пінів
     ce = await cg.gpio_pin_expression(config[CONF_CE_PIN])
@@ -52,6 +56,10 @@ async def to_code(config):
     
     # Налаштування режиму роботи
     cg.add(var.set_mode(config[CONF_MODE]))
+    
+    # Налаштування інтервалу перевірки
+    check_interval = config[CONF_CHECK_INTERVAL]
+    cg.add(var.set_check_interval(check_interval.total_seconds()))
     
     # Налаштування хабів для gateway режиму
     if CONF_HUBS in config:
